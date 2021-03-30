aba55849972963e14603c2758ce0373818f49200
laurens54321 <laurens.hons@gmail.com>
2021-03-30 15:53:14 +0200

    updates

--- src/main/java/com/team13/product/BarbequePartService.java
+++ src/main/java/com/team13/product/BarbequePartService.java
@@ -1,10 +1,12 @@
 package com.team13.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class BarbequePartService {
 
     @Autowired
