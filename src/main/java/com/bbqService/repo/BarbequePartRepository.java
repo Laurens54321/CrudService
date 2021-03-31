9a503069a0397f8adfac52d7c3475fb69ce04bca
laurens54321 <laurens.hons@gmail.com>
2021-03-31 13:50:54 +0200

    secrets removed

--- src/main/java/com/bbqService/repo/BarbequePartRepository.java
+++ src/main/java/com/bbqService/repo/BarbequePartRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface BarbequePartRepository extends CrudRepository<BarbequePart, String> {
-    @Query("SELECT u FROM BarbequePart u WHERE u.partType = ?1")
+    @Query("SELECT u FROM BarbequePart u WHERE u.partType LIKE ?1")
     List<BarbequePart> getPartsOfCertainTypes(String typeString);
 }
